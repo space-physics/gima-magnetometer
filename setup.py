74d4eaf8fd29a10edfd5f84ac9cc1aee8a46df8e
scivision <scivision@users.noreply.github.com>
2017-02-14 11:57:48 -0500

    pythonic-reuse

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-req = ['msp_aurora','sciencedates',
+req = ['sciencedates',
         'nose','numpy','python-dateutil','xarray','matplotlib','seaborn','netcdf4']
 
 setup(name='gima_magnetometer',
       packages=['gimamag'],
-      version = '0.5',
+      version = '0.6',
       description='Plotting and analysis of UAF GIMA Magnetometer data',
       author = 'Michael Hirsch, Ph.D.',
       url = 'https://github.com/scienceopen/gima-magnetometer',
@@ -18,8 +18,5 @@ setup(name='gima_magnetometer',
       'Programming Language :: Python :: 3.6',
       ],
       install_requires=req,
-      dependency_links=[
-      'https://github.com/scienceopen/meridian-spectrometer-reader/tarball/master#egg=msp_aurora'
-      ],
 	  )
 
